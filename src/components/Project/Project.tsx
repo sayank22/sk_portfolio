899b4b366efa8eb312be574f96fc46beccc9cb95
Sayan Kundu <sayank10023@gmail.com>
2026-05-11 10:47:58 +0530

    blank push

--- src/components/Project/Project.tsx
+++ src/components/Project/Project.tsx
@@ -6,7 +6,7 @@ import { motion } from "framer-motion";
 // Project images
 import CodeGeneratorImg from "../../assets/CodeGenerator.png";
 import jivakaImg from "../../assets/jivaka.png";
-import docAIImg from "../../assets/DocAI.png";
+import docAIImg from "../../assets/docai.png";
 import budgetImg from "../../assets/budgetbuddy.png";
 import dropboxImg from "../../assets/dropbox.png";
 import snapshowImg from "../../assets/snapshows.png";
